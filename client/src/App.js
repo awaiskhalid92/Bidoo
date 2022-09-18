e640928ee3a006b0c27c1158a1882bfb8363661c
MuhammadHammadEjaz70 <SP19-BSE-070@cuilahore.edu.pk>
2022-09-18 23:48:51 +0500

    ...

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-const App=()=> {
+import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login";
+import Home from "./pages/Home";
+
+const App = () => {
   return (
-    <div>
-      <p>Bidoo</p>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Login" element={<Login />} />
+        </Routes>
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/nav/Header";
+
+const Home = () => {
+  return (
+    <>
+    <Header/>
+      <p>Home</p>
+    </>
+  );
+};
+
+export default Home;
