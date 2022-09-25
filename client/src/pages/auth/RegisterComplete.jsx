91da1a3b3e5c175bf5edce9b23b44110979acbc6
MuhammadHammadEjaz70 <SP19-BSE-070@cuilahore.edu.pk>
2022-09-25 16:24:38 +0500

    Password less Sign In

--- client/src/pages/auth/RegisterComplete.jsx
+++ client/src/pages/auth/RegisterComplete.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { auth } from "../../firebase";
-import { sendSignInLinkToEmail } from "firebase/auth";
+import { isSignInWithEmailLink, signInWithEmailLink } from "firebase/auth";
 import { toast } from "react-toastify";
 
 const RegisterComplete = ({ history }) => {
@@ -13,12 +13,37 @@ const RegisterComplete = ({ history }) => {
   const handelSubmit = async (e) => {
     e.preventDefault();
     try {
-      const result = await auth.signInWithEmailLink(
-        email,
-        window.location.href
-      );
+      if (isSignInWithEmailLink(auth, window.location.href)) {
+        // Additional state parameters can also be passed via URL.
+        // This can be used to continue the user's intended action before triggering
+        // the sign-in operation.
+        // Get the email if available. This should be available if the user completes
+        // the flow on the same device where they started it.
+        let email = window.localStorage.getItem("emailForSignIn");
+        if (!email) {
+          // User opened the link on a different device. To prevent session fixation
+          // attacks, ask the user to provide the associated email again. For example:
+          email = window.prompt("Please provide your email for confirmation");
+        }
+        // The client SDK will parse the code from the link for you.
+        signInWithEmailLink(auth, email, window.location.href)
+          .then((result) => {
+            // Clear email from storage.
+            window.localStorage.removeItem("emailForSignIn");
+            // You can access the new user via result.user
+            // Additional user info profile not available via:
+            // result.additionalUserInfo.profile == null
+            // You can check if the user is new or existing:
+            // result.additionalUserInfo.isNewUser
+          })
+          .catch((error) => {
+            // Some error occurred, you can inspect the code: error.code
+            // Common errors could be invalid email and invalid or expired OTPs.
+          });
+      }
     } catch (error) {
       toast.error(error.message);
+      console.log(error);
     }
   };
 
